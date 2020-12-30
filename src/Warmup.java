e0a11a7fce9edbd68ec57acec08519279113eb07
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-30 14:23:08 -0600

    add: warmup java class

---
+++ src/Warmup.java
@@ -0,0 +1,30 @@
+public class Warmup {
+//    //Properties
+//    private int num;
+//    private int num1;
+//    private int num2;
+//    private double number1;
+//    private double number2;
+
+    //Write a method called cubed that accepts integer and returns int cubed
+    public static int cubed(int num) {
+        return num * num * num;
+    }
+
+    //Method named subtract. Use method overloading so that you can have subtract take in two ints or two doubles
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static double subtract(double number1, double number2) {
+        return number1 - number2;
+    }
+
+
+    //Main Method
+    public static void main(String[] args) {
+        System.out.println(cubed(5));
+        System.out.println(subtract(5, 3));
+        System.out.println(subtract(5.0, 2.1));
+    }
+}
