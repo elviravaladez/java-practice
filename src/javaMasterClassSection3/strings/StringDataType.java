d9f5f618d8325c69bb63199550f48fbdd577b753
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-08 18:22:10 -0500

    fix: main method in StringDataType class

--- src/javaMasterClassSection3/strings/StringDataType.java
+++ src/javaMasterClassSection3/strings/StringDataType.java
@@ -1,7 +1,7 @@
 package javaMasterClassSection3.strings;
 
 public class StringDataType {
-    public static void main(StringDataType[] args) {
+    public static void main(String[] args) {
         String myString = "This is a string";
 
         System.out.println("myString is equal to " + myString);
