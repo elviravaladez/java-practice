2f38b32b34a55c3a544b0000138876eebc3aee55
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-08 23:24:13 -0500

    feat: SimpleCalculator

--- src/javaMasterClassSection6/Main.java
+++ src/javaMasterClassSection6/Main.java
@@ -6,12 +6,24 @@ public class Main {
         Car ford = new Car();
 
         mazda.setModel("Miata");
-        System.out.println("Model of the car is: " + mazda.getModel());
-        // Model of the car is: Miata
+        System.out.println("Model of the car is: " + mazda.getModel()); // Model of the car is: Miata
 
         mazda.setModel("MX-5 Miata");
-        System.out.println("Model of the car is: " + mazda.getModel());
-        // Model of the car is: Unknown
+        System.out.println("Model of the car is: " + mazda.getModel()); // Model of the car is: Unknown
+
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
+
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+
+        System.out.println("multiply = " + calculator.getMultiplicationResult());
+        System.out.println("divide = " + calculator.getDivisionResult());
 
     }
 }
