00d60757fb827aed1874e8352383bebc0dbb4d76
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-05-09 15:15:30 -0500

    feat: Point

--- src/javaMasterClassSection6/Main.java
+++ src/javaMasterClassSection6/Main.java
@@ -92,5 +92,14 @@ public class Main {
         System.out.println("Width: " + wall.getWidth()); //Width: 5.0
         System.out.println("Height: " + wall.getHeight()); //Height: 0.0
         System.out.println("Area: " + wall.getArea()); //Area: 0.0
+
+        //TODO: Point
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0): " + first.distance()); //distance(0,0): 7.810249675906654
+        System.out.println("distance(second): " + first.distance(second)); //distance(second): 5.0
+        System.out.println("distance(2,2): " + first.distance(2, 2)); //distance(2,2): 5.0
+        Point point = new Point();
+        System.out.println("distance(): " + point.distance()); //distance(): 0.0
     }
 }
